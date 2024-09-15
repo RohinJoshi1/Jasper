15f11c2614e9fe905b4454de3ac96e250bd088c9
Rohin Joshi <rrohiinujoshi@gmail.com>
2024-09-15 06:30:47 +0530

    Adding Visitor Pattern and ASTPrinter

--- src/tools/GenerateAST.java
+++ src/tools/GenerateAST.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public class GenerateAST {
     public static void main(String[] args) throws IOException {
-        if(args.length !=1){
-            System.err.println("Usage: generate_ast <output_directory>");
-            System.exit(64);
-        }
-        String outDir = args[0];
+//        if(args.length !=1){
+//            System.err.println("Usage: generate_ast <output_directory>");
+//            System.exit(64);
+//        }
+        String outDir = "/Users/rohinjoshi/Work/codes/Jalang/src/jalang";
         defineAst(outDir, "Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
@@ -28,16 +28,30 @@ public class GenerateAST {
         pw.println("import java.util.*;");
         pw.println();
         pw.println("abstract class "+baseName + "{");
+        defineVisitor(pw, baseName, types);
         for(String type : types){
             String[] temp = type.split(":");
             String className = temp[0].trim();
             String fields = temp[1].trim();
             defineType(pw,baseName,className,fields);
+
         }
+        pw.println();
+        pw.println("    abstract <R> R accept(Visitor<R> visitor);");
         pw.println("}");
         pw.close();
 
     }
+
+    private static void defineVisitor(PrintWriter pw, String baseName, List<String> types) {
+        pw.println(" interface Visitor<R> {");
+        for(String type: types){
+            String typeName = type.split(":")[0].trim();
+            pw.println(" R visit" + typeName + baseName +"(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+        pw.println("    }");
+    }
+
     private static void defineType(
             PrintWriter writer, String baseName, String className, String fieldList
     ){
@@ -48,6 +62,12 @@ public class GenerateAST {
             String name = field.split(" ")[1];
             writer.println("      this." + name + " = " + name + ";");
         }
+        writer.println("    }");
+        writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
         writer.println("    }");
         writer.println();
         for (String field : fields) {
